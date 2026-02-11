3f861196fe8e8b9335575b169a42253e245e68a4
Pierce Affleck <pierceaffleck@gmail.com>
2026-02-11 03:53:03 +0100

    MC Batch run

--- Flattener/scripts/runNtuplizer.py
+++ Flattener/scripts/runNtuplizer.py
@@ -148,7 +148,7 @@ def main():
     usage = 'usage: %prog [options]'
     parser = optparse.OptionParser(usage)
     parser.add_option('-i', '--in',     dest='input',  help='list of input datasets',    default='listSamplesMC2018.txt', type='string')
-    parser.add_option('-o', '--out',      dest='output',   help='output directory',  default='/eos/user/p/paffleck/prong_analysis', type='string') #EDIT THIS
+    parser.add_option('-o', '--out',      dest='output',   help='output directory',  default='/eos/user/p/paffleck/FlattenerOutput', type='string') #EDIT THIS
     parser.add_option('-f', '--force',      dest='force',   help='force resubmission',  action='store_true')
     parser.add_option('-s', '--submit',   dest='submit',   help='submit jobs',       action='store_true')
     (opt, args) = parser.parse_args()
